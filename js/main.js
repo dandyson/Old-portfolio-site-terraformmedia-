0e967f96b0a09ec9e5137be7e09e742d02792bd0
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-01-15 12:04:14 -0500

    Added a side navigation bar

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let sidebarDiv = document.getElementById("sidebarDiv");
+let navToggle = document.getElementById("navToggle");
+let sidebarCross = document.getElementById("sidebarCross");
+
+navToggle.addEventListener("click", () => {
+    sidebarDiv.style.width = "30%";
+})
+
+sidebarCross.addEventListener("click", () => {
+    sidebarDiv.style.width = "0%";
+})
